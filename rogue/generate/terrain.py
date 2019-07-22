5908d48b019c7efa8fca03c80c5ca33cecc1a063
FranchuFranchu <fff999abc999@gmail.com>
2019-07-21 21:48:35 -0300

    Solved terrain weirdness problem by getting the average of the nearby tiles

--- rogue/generate/terrain.py
+++ rogue/generate/terrain.py
@@ -2,7 +2,8 @@
 from noise import snoise2
 
 def main(seed, mx, my, wx, wy, flatness = 0.4):
-	mx, my = range(mx), range(my)
+	MAP_ROUND = 4
+	mx, my = range(mx * MAP_ROUND), range(my * MAP_ROUND)
 	octaves = 32
 	freq = octaves * 8
 	l = [[0 for i in my] for i in my]
@@ -13,6 +14,19 @@ def main(seed, mx, my, wx, wy, flatness = 0.4):
 			 	y / freq + wy * len(my), 
 			 	octaves,
 			 	base = 0) 
+	fl = []
+	for x in range(len(l) // MAP_ROUND):
+		fl.append([])
+		for y in range(len(l[0]) // MAP_ROUND):
+			avg = 0
+			c = 0
+			for i in range(MAP_ROUND):
+				for j in range(MAP_ROUND):
+					c += 1
+					avg += l[MAP_ROUND * x + i][MAP_ROUND * y + j]
+
+			fl[x].append(avg / c)
+
 	return l
 
 
