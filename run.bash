56ad1be164262bc7b38c8f9962ad4bf4297cd360
FranchuFranchu <fff999abc999@gmail.com>
2019-07-29 22:30:13 -0300

    Fixed bash script again

--- run.bash
+++ run.bash
@@ -3,17 +3,20 @@ command -v python3 -c "" >/dev/null 2>&1 || {
  exit 1;
 }
 all_modules_installed=1
-installed_mods=`pip3 freeze | sed --expression='s/\(.*\)==\(.*\)/\1/'`
-while IFS= read -r line; do
+installed_mods=` pip3 freeze | sed --expression='s/\(.*\)==\(.*\)/\1/' | tr $'\n' $' '`
+
+while read -r line 
+do
 	echo "Checking if $line is installed"
-	python3 -c "import $line" > /dev/null 2>&1
-	if [ $? -eq 1 ] 
-	then
+	
+	echo $is_installed
+	case "$line" in *installed_mods* )
 		echo "$line is not pip-installed!"
 		all_modules_installed=0
 		break
-	fi
-done < <($installed_mods)
+		;;
+	esac
+done < "./requirements.txt"
 if [ $all_modules_installed -eq 1 ]  
 then
 	cd rogue
